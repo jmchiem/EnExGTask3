4a46c8b7e37ecd691156f691ffa83ca08e643002
jmchiem <jenchii08@gmail.com>
2019-03-04 09:37:32 +1100

    Update EnExGTask3.cs

--- EnExGTask3.cs
+++ EnExGTask3.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using Newtonsoft.Json;
 
@@ -16,12 +16,13 @@ public class Msg
 
 public static class EnExGTask3
 {
+    public static string fileName = "saveMsg.json";
 
     public static string getJson(string m)
     {
         Msg tmp;
         string json = string.Empty;
-        string path = Directory.GetCurrentDirectory() + "\\" + "saveMsg.json";
+        string path = Directory.GetCurrentDirectory() + "\\" + fileName;
 
         // if string is null, throw exception
         if (string.IsNullOrEmpty(m))
@@ -65,7 +66,7 @@ public static class EnExGTask3
     public static string getLastMessage()
     {
         string json = string.Empty;
-        string path = Directory.GetCurrentDirectory() + "\\" + "saveMsg.json";
+        string path = Directory.GetCurrentDirectory() + "\\" + fileName;
 
         // if file exists, return last message, else return empty string
         if (File.Exists(path))
@@ -93,8 +94,17 @@ public static class EnExGTask3
         string result = EnExGTask3.getJson(test);
         Console.WriteLine("Test message: " + result);
 
+        test = "second test";
+        result = EnExGTask3.getJson(test);
+        Console.WriteLine("Test message: " + result);
+
         result = EnExGTask3.getLastMessage();
-        Console.WriteLine("Retreive last message: " + result); 
+        Console.WriteLine("Retreive last message: " + result);
+
+        EnExGTask3.fileName = "newFile.json";
+        result = EnExGTask3.getLastMessage();
+        Console.WriteLine("Retreive last message after file change: " + result); 
+
     }
 
 }
