c53a41aefe425f2cd3c1d0c6a76d4e2c3962cf45
Jenny <jmchiem@gmail.com>
2019-03-03 18:56:44 +1100

    2

--- EnExGTask3.cs
+++ EnExGTask3.cs
@@ -4,8 +4,8 @@ using Newtonsoft.Json;
 
 public class Msg
 {
-    string message;
-    string time;
+    public string message;
+    public string time;
 
     public Msg(string m)
     {
@@ -16,14 +16,13 @@ public class Msg
 
 public static class EnExGTask3
 {
-    // store messages in file to retain last message
-    private string path = Directory.GetCurrentDirectory + "\\" + "saveMsg.json";
 
     public static string getJson(string m)
     {
         Msg tmp;
         string json = string.Empty;
-        
+        string path = Directory.GetCurrentDirectory() + "\\" + "saveMsg.json";
+
         // if string is null, throw exception
         if (string.IsNullOrEmpty(m))
         {
@@ -42,7 +41,8 @@ public static class EnExGTask3
         if (tmp == null)
         {
             Console.Error.WriteLine("Message could not be processed.");
-        } else
+        }
+        else
         {
             json = JsonConvert.SerializeObject(tmp);
         }
@@ -53,17 +53,19 @@ public static class EnExGTask3
             StreamWriter sw = new StreamWriter(path);
             sw.WriteLine(json);
             sw.Close();
-        } catch (Exception e)
-        {
-            Console.Error.WriteLine("Message not stored: " + e.ToString);
         }
-        
+        catch (Exception e)
+        {
+            Console.Error.WriteLine("Message not stored: {0}", e);
+        }
+
         return json;
     }
 
     public static string getLastMessage()
     {
         string json = string.Empty;
+        string path = Directory.GetCurrentDirectory() + "\\" + "saveMsg.json";
 
         // if file exists, return last message, else return empty string
         if (File.Exists(path))
@@ -74,13 +76,25 @@ public static class EnExGTask3
                 json = sr.ReadLine();
                 sr.Close();
 
-            } catch(Exception e)
+            }
+            catch (Exception e)
             {
-                Console.Error.WriteLine(e.ToString);
+                Console.Error.WriteLine("{0}", e);
             }
         }
 
         return json;
+    }
+
+    public static void Main()
+    {
+
+        string test = "hello world";
+        string result = EnExGTask3.getJson(test);
+        Console.WriteLine("Test message: " + result);
+
+        result = EnExGTask3.getLastMessage();
+        Console.WriteLine("Retreive last message: " + result); 
     }
 
 }
